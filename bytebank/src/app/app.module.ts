60a15d26e818f6759e140bc96dafe1d9b9954043
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2022-08-01 22:32:51 -0300

    Criando o  1 Componente

---
+++ bytebank/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NovaTranferenciaComponent } from './nova-tranferencia/nova-tranferencia.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NovaTranferenciaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
